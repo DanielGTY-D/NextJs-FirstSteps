7dd9c443d24801d7673f469e74c88b28a7719f48
Yael Torres <yaelgtorres25@gmail.com>
2024-11-10 17:15:56 -0600

    Fin de la sección 2 cap  20/24

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,14 +1,31 @@
 import Link from "next/link";
+import { RiHome3Line } from "react-icons/ri";
+
+interface navItemsProps {
+  path: string,
+  text: string
+}
+
+const navItems : navItemsProps[] = [
+  { path: '/about', text: 'About' },
+  { path: '/pricing', text: 'Pricing' },
+  { path: '/contact', text: 'Contact' },
+]
 
 export const Navbar = () => {
   return (
-    <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
-      <span className="flex-1 text-xl">Home</span>
+    <nav className="flex bg-blue-800 bg-opacity-30 p-3 m-2 rounded">
+      <Link href={"/"} className="flex-1 flex gap-3 items-center"> 
+        <RiHome3Line className="w-7 h-7"/>
+        <span className="text-xl">Home </span>
+      </Link>
 
       <div className="flex gap-4 text-xl">
-        <Link href="/about">About</Link>
-        <Link href="/contact">Contact</Link>
-        <Link href="/pricing">Princing</Link> 
+        {
+          navItems.map( items => 
+              <Link href={items.path} key={items.path}>{items.text}</Link>   
+          )
+        }
       </div>
     </nav>
   );
